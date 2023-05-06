e7746eda2d4154a71da6bdab1bb8cab98cc84933
Arttu Vanninen <avanninen@gmail.com>
2023-05-06 09:42:47 +0300

    Header for file

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+########################################
+# Author:           Arttu Vanninen
+# Version:          not versioned
+# Date:             2023-05-06
+# Description:      General use templates for bash scripts.
+# Usage:            chmod +x test.sh
+#                   ./test.sh    
+########################################
+
 # Import/Source the functions
 . ./functions.sh
 
@@ -11,6 +20,8 @@ set -e # Exit immediately on error
 #test_command
 
 # create_folder "test" "" "test_folder/test4"
+
+#test commit here
 
 echo -e "${RED}Error: ${GREEN}testing ${RESET}"
 exit 0
